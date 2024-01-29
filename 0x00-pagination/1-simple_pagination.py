7df22e5eb508fa83e857eeee8f1e80b3f39a34c3
Mutisomary <marymutso2016@.com>
2024-01-29 01:19:18 -0800

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
-"""pagination"""
-
+""" Pagination """
 import csv
 import math
 from typing import List, Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """return a tuple containing start and end index corresponding to a page"""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
 
 
 class Server:
@@ -32,18 +24,17 @@ class Server:
 
         return self.__dataset
 
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """return a tuple containing start and end index corresponding to a page"""
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+        return start_index, end_index
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """retrieve dataset page and page size"""
-        assert isinstance(page: int) and page > 0, "Page must be an integer."
-        assert isinstance(page_size: int) and page_size > 0,
-        "Page size must be an integer"
-
-        start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-
-        # Check if the start index is within the dataset bounds
-        if start_index >= len(dataset):
-            return []
-
-        # Return the page of the dataset
-        return dataset[start_index:end_index]
+        """
+        Return list of baby names or assert error
+        """
+        assert(isinstance(page, int) and isinstance(page_size, int))
+        assert(page > 0 and page_size > 0)
+        [start_index, end_index] = index_range(page, page_size)
+        return self.dataset()[start_index:end_index]
