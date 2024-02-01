d5fa06ead939835212bbe7e51aa083694faedc28
Mutisomary <marymutso2016@.com>
2024-02-01 02:56:55 -0800

    LFU caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,51 +1,40 @@
-#!/usr/bin/env python3
-"""
-This module contains a LFUCache class that inherits from BaseCaching.
-"""
-
+#!/usr/bin/python3
+""" Python caching systems """
 from base_caching import BaseCaching
-from collections import Counter
+from collections import OrderedDict
+
 
 class LFUCache(BaseCaching):
+    """ LFU caching system
     """
-    LFUCache class. This caching system uses a LFU alg
-    """
-
     def __init__(self):
-        """
-        Initialize the class instance.
-        """
+        """ Initialize class instance. """
         super().__init__()
-        self.keys = []
-        self.counts = Counter()
+        self.cache_data = OrderedDict()
+        self.mru = ""
 
     def put(self, key, item):
+        """ Add an item in the cache
         """
-        Assign to the dictionary self.cache_data the item value for the key
-        """
-        if key is None or item is None:
-            return
-        if key not in self.keys:
-            if len(self.keys) >= BaseCaching.MAX_ITEMS:
-                least_common = self.counts.most_common()[:-2:-1]
-                if len(least_common) > 1:
-                    least_common = [item for item in least_common if item[1] == least_common[0][1]]
-                    least_common = sorted(least_common, key=lambda x: self.keys.index(x[0]))
-                discarded_key = least_common[0][0]
-                self.keys.remove(discarded_key)
-                del self.cache_data[discarded_key]
-                del self.counts[discarded_key]
-                print('DISCARD: {}'.format(discarded_key))
-            self.keys.append(key)
-        self.cache_data[key] = item
-        self.counts[key] += 1
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.cache_data.update({key: item})
+                    self.mru = key
+                else:
+                    # discard the most recently used item
+                    discarded = self.mru
+                    del self.cache_data[discarded]
+                    print("DISCARD: {}".format(discarded))
+                    self.cache_data[key] = item
+                    self.mru = key
+            else:
+                self.cache_data[key] = item
+                self.mru = key
 
     def get(self, key):
+        """ Get an item by key
         """
-        Return the value in self.cache_data linked to key.
-        """
-        if key is None or key not in self.cache_data:
-            return None
-        self.counts[key] += 1
-        return self.cache_data[key]
-
+        if key in self.cache_data:
+            self.mru = key
+            return self.cache_data[key]
