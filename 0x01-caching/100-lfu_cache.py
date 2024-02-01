af67aed5bca2f68142dbc54141c040317cf8792e
Mutisomary <marymutso2016@.com>
2024-02-01 02:50:12 -0800

    LFU caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,40 +1,53 @@
 #!/usr/bin/python3
-""" Python caching systems """
+""" LFUCache module to work with a basic dictionary using LFU algorithm.
+"""
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
-    """ LFU caching system
+    """ LFUCache inherits from BaseCaching and
+    is a caching system using LFU algorithm.
     """
+
     def __init__(self):
-        """ Initialize class instance. """
+        """ Initialize
+        """
         super().__init__()
-        self.cache_data = OrderedDict()
-        self.mru = ""
+        self.frequency = {}
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """ Add an item in the cache using LFU algorithm
         """
-        if key and item:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                if key in self.cache_data:
-                    self.cache_data.update({key: item})
-                    self.mru = key
-                else:
-                    # discard the most recently used item
-                    discarded = self.mru
-                    del self.cache_data[discarded]
-                    print("DISCARD: {}".format(discarded))
-                    self.cache_data[key] = item
-                    self.mru = key
+        if key is None or item is None:
+            return
+
+        dict_data = self.cache_data
+        if len(dict_data) >= BaseCaching.MAX_ITEMS:
+            # If the cache is full, discard the least frequency used item (LFU)
+            lfu_keys = [
+            k for k, v in self.frequency.items() if v == min(self.frequency.values())
+            ]   
+            if len(lfu_keys) > 1:
+                # If more than 1 item to discard, use LRU algo to discard LRU
+                lfu_key = self.order_used.pop(0)
             else:
-                self.cache_data[key] = item
-                self.mru = key
+                lfu_key = lfu_keys[0]
+            del dict_data[lfu_key]
+            del self.frequency[lfu_key]
+            print("DISCARD:", lfu_key)
+
+        # Add the new item to the cache and update frequency
+        dict_data[key] = item
+        self.frequency[key] = self.frequency.get(key, 0) + 1
 
     def get(self, key):
         """ Get an item by key
         """
-        if key in self.cache_data:
-            self.mru = key
-            return self.cache_data[key]
+        dict_data = self.cache_data
+        if key is None or key not in dict_data:
+            return None
+
+        # Update frequency to indicate recent usage
+        self.frequency[key] += 1
+
+        return dict_data[key]
