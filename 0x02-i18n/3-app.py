aa7e6fc214acfde9779a5f94505d88518fd68f04
Mutisomary <marymutso2016@gmail.com>
2024-02-06 17:26:39 +0300

    flask app

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""simple flask application"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config file"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Using the Config class as the configuration for the Flask app
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+@babel.localeselector
+def get_locale():
+    """function determines the best match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello_world():
+    """the home page function"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
